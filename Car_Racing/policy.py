f177091e99530963e89452c9553c1ae77a1a41a3
Qiu Jumin <61496652+qiujumin@users.noreply.github.com>
2023-08-28 16:45:58 +0800

    Add files via upload

--- Car_Racing/policy.py
+++ Car_Racing/policy.py
@@ -5,15 +5,15 @@ from stable_baselines3.common.evaluation import evaluate_policy
 
 env = gym.make("CarRacing-v0")
 
-# model = PPO("CnnPolicy", env, verbose=1)
-# model.learn(total_timesteps=int(2e5), progress_bar=True)
-# model.save("data/ppo_carracing")
-# del model
+model = PPO("CnnPolicy", env, verbose=1)
+model.learn(total_timesteps=int(2e5), progress_bar=True)
+model.save("data/ppo_carracing")
+del model
 
 model = PPO.load("data/ppo_carracing", env=env)
 
-# mean_reward, std_reward = evaluate_policy(
-#     model, model.get_env(), n_eval_episodes=10)
+mean_reward, std_reward = evaluate_policy(
+    model, model.get_env(), n_eval_episodes=10)
 
 vec_env = model.get_env()
 obs = vec_env.reset()
