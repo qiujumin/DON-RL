101b2d33029caa08e13563089a0e1281bf5edfb0
Qiu Jumin <61496652+qiujumin@users.noreply.github.com>
2023-10-08 11:39:22 +0800

    Update train.py

--- Tic-Tac-Toe/train.py
+++ Tic-Tac-Toe/train.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 from stable_baselines3 import PPO
 from torchvision import transforms
 
-layer1 = nn.Parameter(torch.normal(0, 1, size=(Ny, Nx)))
-layer2 = nn.Parameter(torch.normal(0, 1, size=(Ny, Nx)))
-layer3 = nn.Parameter(torch.normal(0, 1, size=(Ny, Nx)))
+layer = nn.ParameterList(nn.Parameter(torch.normal(0, 1, size=(Ny, Nx))) for i in range(3))
 
 model = DON(layer1, layer2, layer3).to(device)
 criterion = torch.nn.MSELoss(reduction="sum")
@@ -58,4 +56,4 @@ for step in range(1000):
             phase = np.where(phase > np.pi, phase - 2 * np.pi, phase)
             phase = np.where(phase < -np.pi, phase + 2 * np.pi, phase)
 
-            plt.imsave(f"data/layer{_+1}.png", phase, cmap="gray_r")
+            np.savetxt(f"data/phase{i}.csv", phase, delimiter=",")
